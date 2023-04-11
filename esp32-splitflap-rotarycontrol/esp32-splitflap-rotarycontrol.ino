776683437903e08b044a45e16cf61d5cf7c77ebd
Andrew Ry <RyAndrew@users.noreply.github.com>
2023-04-11 02:35:09 -0500

    add web ui with websockets, read hall sensor

--- esp32-splitflap-rotarycontrol/esp32-splitflap-rotarycontrol.ino
+++ esp32-splitflap-rotarycontrol/esp32-splitflap-rotarycontrol.ino
@@ -3,6 +3,14 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "FS.h"
+#include <LittleFS.h>
+
+#include <AsyncJson.h>
+#include <ArduinoJson.h>
+
 #include <debounce.h>
 #include <RotaryEncoder.h>
 
@@ -11,24 +19,28 @@
 #define ROTARY_ENCODER_BUTTON_PIN 21
 #define ROTARY_ENCODER_STEPS 4
 
+#define FLAP1_HALL 36
+
 #define LED 2
 
 #define MOTOR_DIR_PIN 12
 #define MOTOR_STEP_PIN 13
 
-const int MOTOR_INCREMENT = 15;
-const int MOTOR_STEP_TIMER = 1500;
+const int MOTOR_INCREMENT = 30;
+const int MOTOR_STEP_TIMER = 950;
 
 volatile long motorMoveDistance = 0;
+
+RotaryEncoder *encoder = nullptr;
 
 //WIFI
 const char* ssid = "wifissid";
 const char* password = "wifipassword";
 
-RotaryEncoder *encoder = nullptr;
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 hw_timer_t *Motor_timer = NULL;
-
 void IRAM_ATTR runMotor(){
 
   if(digitalRead(MOTOR_STEP_PIN)){
@@ -64,6 +76,7 @@ static void buttonHandler(uint8_t btnId, uint8_t btnState) {
     digitalWrite(LED,!digitalRead(LED));
     motorMoveDistance=0;
     encoder->setPosition(0);
+    ws.textAll("Button Pressed");
   } else {
     Serial.println("Released button");
   }
@@ -71,13 +84,185 @@ static void buttonHandler(uint8_t btnId, uint8_t btnState) {
 
 static Button myButton(0, buttonHandler);
 
+static void hallHandler(uint8_t btnId, uint8_t btnState) {
+  if (btnState == BTN_PRESSED) {
+    Serial.println("Hall Sensor!");
+    ws.textAll("Hall Sensor!");
+  } else {
+    ws.textAll("Released Hall Sensor!");
+    Serial.println("Released Hall Sensor!");
+  }
+}
+
+static Button flap1_hall(0, hallHandler);
+
 void IRAM_ATTR checkPosition() {
   encoder->tick(); // just call tick() to check the state.
+}
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+
+
+void handleClientJsonData(String data){
+  StaticJsonDocument<200> decodedJson;
+  
+  DeserializationError errorDecode = deserializeJson(decodedJson, data);
+  // Test if parsing succeeds.
+  if (errorDecode) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(errorDecode.c_str());
+    return;
+  }
+  //JsonObject decodedJsonObject = decodedJsonString.to<JsonObject>();
+    JsonVariant jsonCmd = decodedJson["cmd"];
+    if (jsonCmd.isNull()) {
+      Serial.println("cmd not found");
+      return;
+    }
+    Serial.println("json cmd received!");
+    String cmdString = jsonCmd.as<String>();
+
+    Serial.println(cmdString);
+    
+    if(cmdString == "ledtoggle"){
+      digitalWrite(LED,!digitalRead(LED));
+      return;
+    }
+    if(cmdString == "motorstep10"){
+      motorMoveDistance += 10;
+      return;
+    }
+    if(cmdString == "motorstep20"){
+      motorMoveDistance += 20;
+      return;
+    }
+    if(cmdString == "motorspeed1"){
+      motorspeed1();
+      return;
+    }
+    if(cmdString == "motorspeed2"){
+      motorspeed2();
+      return;
+    }
+    if(cmdString == "motorspeed3"){
+      motorspeed3();
+      return;
+    }
+    
+    /*
+    if(cmdString == "targetUp"){
+      JsonVariant jsonValue = decodedJson["value"];
+      if (jsonValue.isNull()){
+        Serial.println("targetUp value not found");
+        return;
+      }
+      if(jsonValue.as<String>() == "all"){
+        Serial.println("command targetUp = all");
+        putAllTargetsUp();
+        return;
+      }else{
+       int targetNo = jsonValue.as<unsigned int>();
+       if(targetNo == 0 || targetNo > 6){
+        Serial.print("command targetUp value invalid");
+        return;
+        }
+        Serial.print("command targetUp = ");
+        Serial.println(targetNo);
+        allTargets[targetNo-1].servoResetCounter = 4;
+      }
+    }
+    */
+}
+
+
+void onEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+    //client connected
+    Serial.printf("ws[%s][%u] connect\n", server->url(), client->id());
+    //client->printf("Hello Client %u :)", client->id());
+    client->ping();
+  } else if(type == WS_EVT_DISCONNECT){
+    //client disconnected
+    Serial.printf("ws[%s][%u] disconnect: %u\n", server->url(), client->id());
+  } else if(type == WS_EVT_ERROR){
+    //error was received from the other end
+    Serial.printf("ws[%s][%u] error(%u): %s\n", server->url(), client->id(), *((uint16_t*)arg), (char*)data);
+  } else if(type == WS_EVT_PONG){
+    //pong message was received (in response to a ping request maybe)
+    Serial.printf("ws[%s][%u] pong[%u]: %s\n", server->url(), client->id(), len, (len)?(char*)data:"");
+  } else if(type == WS_EVT_DATA){
+    //data packet
+    AwsFrameInfo * info = (AwsFrameInfo*)arg;
+    if(info->final && info->index == 0 && info->len == len){
+      //the whole message is in a single frame and we got all of it's data
+      Serial.printf("ws[%s][%u] %s-message[%llu]: ", server->url(), client->id(), (info->opcode == WS_TEXT)?"text":"binary", info->len);
+        String msg = "";
+      if(info->opcode == WS_TEXT){
+        data[len] = 0;
+        Serial.printf("%s\n", (char*)data);
+          msg = (char*)data;
+      } else {
+        for(size_t i=0; i < info->len; i++){
+          Serial.printf("%02x ", data[i]);
+        }
+        Serial.printf("\n");
+      }
+      if(info->opcode == WS_TEXT){
+        //client->text("I got your text message");
+        handleClientJsonData(msg);
+      }else{
+        client->binary("I got your binary message");
+      }
+    } else {
+      //message is comprised of multiple frames or the frame is split into multiple packets
+      if(info->index == 0){
+        if(info->num == 0)
+          Serial.printf("ws[%s][%u] %s-message start\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+          Serial.printf("ws[%s][%u] frame[%u] start[%llu]\n", server->url(), client->id(), info->num, info->len);
+      }
+
+      Serial.printf("ws[%s][%u] frame[%u] %s[%llu - %llu]: ", server->url(), client->id(), info->num, (info->message_opcode == WS_TEXT)?"text":"binary", info->index, info->index + len);
+      
+      String msg = "";
+      if(info->message_opcode == WS_TEXT){
+        data[len] = 0;
+        Serial.printf("%s\n", (char*)data);
+        msg.concat((char*)data);
+      } else {
+        for(size_t i=0; i < len; i++){
+          Serial.printf("%02x ", data[i]);
+        }
+        Serial.printf("\n");
+      }
+
+      if((info->index + len) == info->len){
+        Serial.printf("ws[%s][%u] frame[%u] end[%llu]\n", server->url(), client->id(), info->num, info->len);
+        if(info->final){
+          Serial.printf("ws[%s][%u] %s-message end\n", server->url(), client->id(), (info->message_opcode == WS_TEXT)?"text":"binary");
+          if(info->message_opcode == WS_TEXT){
+            //client->text("I got your text message");
+            handleClientJsonData(msg);
+          }
+          //else{
+            //client->binary("I got your binary message");
+          //}
+        }
+      }
+    }
+  }
+}
+void initWebSocket() {
+  ws.onEvent(onEvent);
+  server.addHandler(&ws);
 }
 
 void setup() {
   
   pinMode(LED,OUTPUT);
+
+  pinMode(FLAP1_HALL, INPUT_PULLUP);
 
   // use TWO03 mode when PIN_IN1, PIN_IN2 signals are both LOW or HIGH in latch position.
   encoder = new RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, RotaryEncoder::LatchMode::TWO03);
@@ -101,16 +286,32 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  InitOta();
-
+  // while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+  //   Serial.println("Connection Failed! Rebooting...");
+  //   delay(5000);
+  //   ESP.restart();
+  // }
+  WiFi.waitForConnectResult();
   Serial.println("Ready");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  InitOta();
+  initWebSocket();
+  
+  if (!LittleFS.begin()){
+    Serial.println("An Error has occurred while mounting LittleFS");
+  }
+  
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+  server.serveStatic("/", LittleFS, "/");
+
+  server.onNotFound(notFound);
+
+  server.begin();
+
 
   //LED on when boot finished
   digitalWrite(LED,HIGH);
@@ -121,6 +322,15 @@ void setup() {
   timerAlarmEnable(Motor_timer);
 }
 
+void motorspeed1( ) {
+  timerAlarmWrite(Motor_timer, MOTOR_STEP_TIMER, true);
+}
+void motorspeed2( ) {
+  timerAlarmWrite(Motor_timer, MOTOR_STEP_TIMER - 50, true);
+}
+void motorspeed3( ) {
+  timerAlarmWrite(Motor_timer, MOTOR_STEP_TIMER - 100, true);
+}
 void InitOta( ) {
 
   // Port defaults to 3232
@@ -171,6 +381,8 @@ void loop() {
 
   ArduinoOTA.handle();
 
+  flap1_hall.update(digitalRead(FLAP1_HALL));
+
   myButton.update(digitalRead(ROTARY_ENCODER_BUTTON_PIN));
   static int pos = 0;
   encoder->tick();
@@ -189,9 +401,11 @@ void loop() {
     if(dir == RotaryEncoder::Direction::COUNTERCLOCKWISE){
       motorMoveDistance += (-1 * MOTOR_INCREMENT);
       // Serial.println("Turning CCW");
+      ws.textAll("Turning CCW");
     }else{
       motorMoveDistance += MOTOR_INCREMENT;
       // Serial.println("Turning CW");
+      ws.textAll("Turning CW");
     }
     // Serial.printf("mMD = %ld\r\n", motorMoveDistance );
     digitalWrite(LED,HIGH);
